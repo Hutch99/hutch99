29c15e040190550eb61c3660e6b5fbb8a0ece362
Hutch <hutch@hutchresources.com>
2022-06-02 19:13:23 -0700

    added ball bounce, colors,read more and buttons

--- assets/js/global.js
+++ assets/js/global.js
@@ -77,4 +77,63 @@ readMore.addEventListener('click', showInfo);
 //document.getElementById('bingoreset').onclick = createBingoCard;
 
 onload = createBingoCard;
+let colorButton = document.getElementById('color-button');
 
+// This variable stores the "Mystery Color" button
+let scrollButton = document.getElementById('next-button');
+
+// This random number function will create color codes for the randomColor variable
+function colorValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function colorChange(event){
+let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
+event.target.style.backgroundColor = randomColor;
+}
+
+colorButton.addEventListener('click',colorChange);
+scrollButton.onwheel = colorChange;
+
+
+let itemOne = document.getElementById('list-item-one');
+let itemTwo = document.getElementById('list-item-two');
+let itemThree = document.getElementById('list-item-three');
+let itemFour = document.getElementById('list-item-four');
+let itemFive = document.getElementById('list-item-five');
+let resetButton = document.getElementById('reset-button');
+// This function programs the "Reset" button to return the boxes to their default styles
+let reset = function() {
+	itemOne.style.width = ''
+	itemTwo .style.backgroundColor = ''
+	itemThree.innerHTML = 'The mouse must leave the box to change the text'
+	itemFive.style.display = "none"
+  };
+  resetButton.onclick = reset;
+  
+  function increaseWidth(){
+	itemOne.style.width = '600px';
+  }
+  function changeBackground(){
+	itemTwo.style.backgroundColor = "red";
+  }
+  function changeText(){
+	itemThree.innerHTML = "The mouse has left the element";
+  }
+  function showItem(){
+	itemFive.style.display = 'block';
+  }
+  itemTwo.addEventListener('mouseup',changeBackground);
+  itemOne.addEventListener('mouseover', increaseWidth);
+  itemThree.addEventListener('mouseout',changeText);
+  itemFour.addEventListener('mousedown', showItem);
+
+let ball = document.getElementById('float-circle');
+function up(){
+  ball.style.bottom = '250px';
+}
+function down(){
+  ball.style.bottom = '50px';
+}
+document.addEventListener('keydown', up);
+document.addEventListener('keyup', down);
